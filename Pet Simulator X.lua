6a6ba6e3e9eac1b45fc505523fb42b9c247d6b80
Dux <106892066+DuxV2@users.noreply.github.com>
2023-01-26 17:01:59 -0500

    Update Pet Simulator X.lua

--- Pet Simulator X.lua
+++ Pet Simulator X.lua
@@ -7,13 +7,20 @@ local QuadHatch = false
 
 -- LOCALS --
 
--- local tab = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
--- local tab2 = win:Tab("Auto-Farm", "http://www.roblox.com/asset/?id=6022668888")
-
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Win = OrionLib:MakeWindow({Name = "Pet Simulator X", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local Tab = Win:MakeTab({Name = "Eggs",Icon = "rbxassetid://4483345998",PremiumOnly = false})
+local Tab = Win:MakeTab({Name = "Main",Icon = "rbxassetid://4483345998",PremiumOnly = false})
+local Tab2 = Win:MakeTab({Name = "Eggs",Icon = "rbxassetid://6023426915",PremiumOnly = false})
+local Tab3 = Win:MakeTab({Name = "Misc",Icon = "rbxassetid://6022668888",PremiumOnly = false})
 local Eggs = { "None" }
+
+local Network = require(game:GetService("ReplicatedStorage").Library.Client.Network)
+local Fire, Invoke = Network.Fire, Network.Invoke
+
+local old
+old = hookfunction(getupvalue(Fire, 1), function(...)
+    return true
+end)
 
 -- FUNCTIONS
 
@@ -24,15 +31,11 @@ function grabEggs(world)
 end
 
 function openEgg(egg, triple, quad)
-    local Network = require(game:GetService("ReplicatedStorage").Library.Client.Network)
-    local Fire, Invoke = Network.Fire, Network.Invoke
-
-    local old
-    old = hookfunction(getupvalue(Fire, 1), function(...)
-        return true
-    end)
-
     Network.Invoke("Buy Egg", egg, triple, quad)
+end
+
+function toggleHoverboard(state)
+    Network.Fire("Update Hoverboard State", state)
 end
 
 grabEggs("Spawn Eggs")
@@ -45,9 +48,9 @@ grabEggs("Doodle Eggs")
 
 -- BUTTONS --
 
-Tab:AddLabel("Disclaimer: Must be in world where the egg is to work!")
+Tab2:AddLabel("Disclaimer: Must be in world where the egg is to work!")
 
-Tab:AddToggle({
+Tab2:AddToggle({
 	Name = "Auto Hatch Egg",
 	Default = false,
 	Callback = function(Value)
@@ -56,7 +59,7 @@ Tab:AddToggle({
 	end    
 })
 
-Tab:AddDropdown({
+Tab2:AddDropdown({
 	Name = "Egg To Auto Hatch",
 	Default = "None",
 	Options = Eggs,
@@ -65,7 +68,7 @@ Tab:AddDropdown({
 	end    
 })
 
-Tab:AddDropdown({
+Tab2:AddDropdown({
 	Name = "Opening Type",
 	Default = "Single",
 	Options = {"Single", "Triple", "Quad"},
@@ -83,6 +86,14 @@ Tab:AddDropdown({
 	end    
 })
 
+Tab3:AddToggle({
+	Name = "Toggle Hoverboard",
+	Default = false,
+	Callback = function(Value)
+		toggleHoverboard(Value)
+	end    
+})
+
 -- LOOPS --
 
 while wait(0.25) do
@@ -90,3 +101,4 @@ while wait(0.25) do
         openEgg(EggToAutoHatch, TripleHatch, QuadHatch)
     end
 end
+
