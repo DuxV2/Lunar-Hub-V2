cc1f5a4cf758a2c16682938bf5dd5bb054097153
Dux <106892066+DuxV2@users.noreply.github.com>
2023-01-31 10:10:16 -0500

    Update and rename Network.lua to Utils.lua

--- Network/Network.lua
+++
@@ -1,8 +0,0 @@
-local Utils = {
-    Pathfinding = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/PathfindingLib.lua")))();
-    Network = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/Network.lua")))();
-    Webhook = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/WebhookLib.lua")))();
-    Suffix = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/SuffixLib.lua")))();
-    Table = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/TableLib.lua")))();
-};
-return Utils
---
+++ Network/Utils.lua
@@ -0,0 +1,8 @@
+local Utils = {
+    Pathfinding = loadstring(game:HttpGet(("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Network/Utils/Pathfindinglib.lua?token=GHSAT0AAAAAAB6ALCWLP46IZ3KBMRLDEZCSY6ZF43A")))();
+    Network = loadstring(game:HttpGet(("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Network/Utils/Network.lua?token=GHSAT0AAAAAAB6ALCWKOFNXSSL3A6JOYASAY6ZF4JQ")))();
+    Webhook = loadstring(game:HttpGet(("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Network/Utils/Webhooklib.lua?token=GHSAT0AAAAAAB6ALCWLAKXNCAZDGYU2MUPKY6ZF6BA")))();
+    Suffix = loadstring(game:HttpGet(("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Network/Utils/Suffixlib.lua?token=GHSAT0AAAAAAB6ALCWKXIPF5GOEJ5X7EBTEY6ZF5FA")))();
+    Table = loadstring(game:HttpGet(("hhttps://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Network/Utils/Tablelib.lua?token=GHSAT0AAAAAAB6ALCWKTIWLQXWVAVH4VYROY6ZF53Q")))();
+};
+return Utils
