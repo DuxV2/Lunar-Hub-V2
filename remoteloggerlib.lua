c526670d62fd5a6f125abe1304b9b9bb6623929f
Dux <106892066+DuxV2@users.noreply.github.com>
2023-01-31 14:08:46 -0500

    Update remoteloggerlib.lua

--- remoteloggerlib.lua
+++ remoteloggerlib.lua
@@ -32,3 +32,29 @@ MT.__namecall =
         return NC(obj, ...)
     end
 )
+
+-- newer
+ 
+local old
+old =
+    hookmetamethod(
+    game,
+    "__namecall",
+    function(self, ...)
+        local args = {...}
+        local event = self
+        local namecallmethod = getnamecallmethod()
+        if namecallmethod == "FireServer" or namecallmethod == "InvokeServer" then
+            rconsoleprint("-------------------------\n")
+            rconsoleprint("REMOTE EVENT\n")
+            rconsoleprint("event: " .. self:GetFullName() .. "\n")
+            rconsoleprint("method: " .. getnamecallmethod() .. "\n")
+            rconsoleprint("args:\n")
+            for i, v in pairs(args) do
+                rconsoleprint("- " .. tostring(v) .. "\n")
+            end
+            rconsoleprint("-------------------------\n")
+        end
+        return old(self, ...)
+    end
+)
