048d9e3c0c028d91831c6df3f973d178fb7752a7
Dux <106892066+DuxV2@users.noreply.github.com>
2023-02-02 17:59:50 -0500

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,19 +1,19 @@
 if game.PlaceId == 9285238704 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Race%20Clicker.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Race%20Clicker.lua"))()
 elseif game.PlaceId == 6918802270 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Project%20New%20World.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Project%20New%20World.lua"))()
 elseif game.PlaceId == 6516141723 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/DOORS.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/DOORS.lua"))()
 elseif game.PlaceId == 11542692507 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Anime%20Souls%20Simulator.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Anime%20Souls%20Simulator.lua"))()
 elseif game.PlaceId == 8579989858 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Super%20Clicker%20Simulator.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Super%20Clicker%20Simulator.lua"))()
 elseif game.PlaceId == 11400511154 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Monkey%20Tycoon.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Monkey%20Tycoon.lua"))()
 elseif game.PlaceId == 11800876530 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Every%20Second%20You%20Get%20%2B1%20Blocks.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Every%20Second%20You%20Get%20%2B1%20Blocks.lua"))()
 elseif game.PlaceId == 11063612131 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Every%20Second%20You%20Get%20%2B1%20Jump.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Every%20Second%20You%20Get%20%2B1%20Jump.lua"))()
 elseif game.PlaceId == 8737602449 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Race%20Clicker.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Race%20Clicker.lua"))()
 end
