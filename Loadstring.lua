edc3c112a629e484f7b2424f00747185666c70c2
Dux <106892066+DuxV2@users.noreply.github.com>
2023-02-05 12:35:34 -0500

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -15,7 +15,7 @@ elseif game.PlaceId == 11800876530 then
 elseif game.PlaceId == 11063612131 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Every%20Second%20You%20Get%20%2B1%20Jump.lua"))()
 elseif game.PlaceId == 8737602449 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/Race%20Clicker.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxV2/Lunar-Hub-V2/main/Games/Featured/PLS%20DONATE.lua"))()
 end
 
 if game.PlaceId ~= 9285238704 and game.PlaceId ~= 6918802270 and  game.PlaceId ~= 6516141723 and  game.PlaceId ~= 11542692507 and  game.PlaceId ~= 11400511154 and  game.PlaceId ~= 11800876530 and  game.PlaceId ~= 11063612131 and  game.PlaceId ~= 8737602449 then
